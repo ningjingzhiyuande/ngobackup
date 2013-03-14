bbd00c8e793ec03fca8b7633fa18c6037c3fa219
shiguodong <oreatial@gmail.com>
2013-03-14 17:04:12 +0800

    add

---
+++ app/controllers/ngo_controller.rb
@@ -0,0 +1,4 @@
+class NgoController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itbackup::Application.config.secret_token = '3796ea285d072db17e8bee4ef48b19d32f82c6ada568dcba06945b746609f88e1489b3214721f3eddce981fbcebae587a54156dabf44cea252956e2061dca6bb'
