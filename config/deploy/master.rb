f0189e9c9c29d687525bc0f96a85b48ded706fbc
shiguodong <oreatial@gmail.com>
2013-05-31 17:15:45 +0800

    add

--- config/deploy/master.rb
+++ config/deploy/master.rb
@@ -3,10 +3,10 @@ set :rails_env, 'production'
 set :branch, 'master'
 
 # 代码发布服务器
-role :app, '42.96.169.241'
+role :app, '110.76.44.69'
 
 # 网站服务器
-role :web, '42.96.169.241'
+role :web, '110.76.44.69'
 
 # 使用数据库服务器运行迁移
-role :db,  '42.96.169.241', primary: true, no_release: true
+role :db,  '110.76.44.69', primary: true, no_release: true
