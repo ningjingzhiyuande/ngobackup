3298072de95e364c45f5e61b6ebb1929e97501ae
shiguodong <oreatial@gmail.com>
2013-06-06 17:41:44 +0800

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,7 +81,7 @@ namespace :deploy do
   end
 
   task :compile_assets, :roles => :web do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env}  #{rake}  assets:precompile"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env}  #{rake} assets:precompile --trace"
   end
 
   task :clean_git_cache, :roles => :app do
