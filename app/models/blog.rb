f5ce0feb1124d883a0828c8b2bb43cc1c27b4d74
shiguodong <oreatial@gmail.com>
2013-06-20 15:36:18 +0800

    add

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ActiveRecord::Base
   attr_accessible :category_id, :content, :tags, :title, :user_id,:is_top,:status,:cover
   as_enum :category, ["分享", "农业", "科技"],:column=>:category_id
-  blongs_to :user
+  belongs_to :user
   mount_uploader :cover,  ::CoverUploader
 end
